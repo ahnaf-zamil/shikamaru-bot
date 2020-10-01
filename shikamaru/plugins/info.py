ebc70975bade9e4a7dd1a19e2aed8d5cde98101e
K.M Ahnaf Zamil <ahnafzamil896@gmail.com>
2020-10-01 16:54:16 +0600

    Added translate command using Google Translate API

--- shikamaru/plugins/info.py
+++ shikamaru/plugins/info.py
@@ -3,22 +3,23 @@ import hikari
 import datetime
 import psutil
 import time
+import os
 import aiofiles
 import sqlite3
 import itertools
 import datetime
-from platform import python_version
+import platform
+import googletrans
 
 
 start_time = datetime.datetime.utcnow()
 
-class Info(lightbulb.Plugin):
 
+class Info(lightbulb.Plugin):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
         self.process = psutil.Process()
-
 
     @lightbulb.command()
     async def ping(self, ctx):
@@ -30,7 +31,9 @@ class Info(lightbulb.Plugin):
         heartbeat = self.bot.heartbeat_latency * 1000
         em = hikari.Embed(title=":ping_pong: Pong! :ping_pong:", color="#00a136")
         em.add_field(name="REST API", value=f"`{int(millis):,.2f}ms`", inline=True)
-        em.add_field(name="Websocket Gateway", value=f"`{int(heartbeat):,.2f}ms`", inline=True)
+        em.add_field(
+            name="Websocket Gateway", value=f"`{int(heartbeat):,.2f}ms`", inline=True
+        )
         em.set_footer(text=str(self.bot.me), icon=self.bot.me.avatar_url)
         em.timestamp = datetime.datetime.now().astimezone()
         await msg.delete()
@@ -50,38 +53,38 @@ class Info(lightbulb.Plugin):
             )
         else:
             time_format = "**{h}** hours, **{m}** minutes, and **{s}** seconds."
-        uptime_stamp = time_format.format(
-            d=days, h=hours, m=minutes, s=seconds)
-        embed = hikari.Embed()
-        embed.title = 'About Shikamaru'
-        embed.colour = "#326fa8"
-        embed.url = "https://github.com/ahnaf-zamil/shikamaru-bot"
+        uptime_stamp = time_format.format(d=days, h=hours, m=minutes, s=seconds)
+        embed = hikari.Embed(
+            title="About Shikamaru",
+            color="#326fa8",
+            url="https://github.com/ahnaf-zamil/shikamaru-bot",
+        )
         owner = await self.bot.rest.fetch_user(int(self.bot.owner_ids[0]))
         embed.set_thumbnail(self.bot.me.avatar_url)
         embed.add_field(name="Owner", value=f":point_right: {owner} :point_left:")
         # Statistics
-        total_members = 0
-        chn = 0
-        guilds = 0
-        for guild in self.bot.cache.get_available_guilds_view().values():
-            guilds += 1
-            total_members += guild.member_count
-            for channel in guild.channels:
-                chn += 1
         t = t = f"""```py
-Python Version: {python_version()}
+Python Version: {platform.python_version()}
 Hikari Version: {hikari.__version__}
-SQLite3 Version: {sqlite3.version}
 ```
         """
-        embed.add_field(name='Users', value=f'{total_members}', inline=True)
-        embed.add_field(name='Servers', value=f'{len([server async for server in self.bot.rest.fetch_my_guilds()])}', inline=True)
-        embed.add_field(name='Channels', value=f'{chn} total', inline=True)
-        memory_usage = self.process.memory_full_info().uss / 1024**2
+        osinfo = platform.uname()
+        pid = os.getpid()
+        py = psutil.Process(pid)
+        memoryUse = (py.memory_info()[0] / 2.0 ** 30) * 1024
+        svmem = psutil.virtual_memory()
+        totalmemory = svmem.total // 1000000
         cpu_usage = self.process.cpu_percent() / psutil.cpu_count()
-        embed.add_field(name='Process', value=f'```Memory: {memory_usage:.2f} MiB\nCPU: {cpu_usage:.2f}%```', inline=True)
+        embed.add_field(
+            name="OS", value=f"{osinfo.system} {osinfo.version} {osinfo.machine}"
+        )
+        embed.add_field(
+            name="Process",
+            value=f"```Memory: {round(memoryUse/totalmemory * 100, 2):.2f} MiB\nCPU: {psutil.cpu_percent(interval=None, percpu=False)}%```",
+            inline=True,
+        )
         version = hikari.__version__
-        embed.add_field(name='Uptime', value=uptime_stamp)
+        embed.add_field(name="Uptime", value=uptime_stamp)
         embed.add_field(name="Libraries", value=t)
         embed.set_footer(text=str(self.bot.me), icon=self.bot.me.avatar_url)
         embed.timestamp = datetime.datetime.now().astimezone()
@@ -89,15 +92,16 @@ SQLite3 Version: {sqlite3.version}
 
     @lightbulb.command()
     async def github(self, ctx):
-        '''
+        """
         Gives link to Shikamaru's GitHub repository
-        '''
+        """
         em = hikari.Embed(title="Shikamaru's GitHub", color="#4dffa6")
         em.description = "[Click here](https://github.com/ahnaf-zamil/shikamaru-bot) to go to Shikamaru's GitHub page"
         em.set_footer(text=str(self.bot.me), icon=self.bot.me.avatar_url)
         em.timestamp = datetime.datetime.now().astimezone()
         em.set_footer(text=str(self.bot.me), icon=self.bot.me.avatar_url)
         await ctx.reply(embed=em)
+
 
 def load(bot):
     bot.add_plugin(Info(bot))
