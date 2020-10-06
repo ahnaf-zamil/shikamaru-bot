fe86b5fbebf9aa38839d3399acdc1c198a664573
K.M Ahnaf Zamil <ahnafzamil896@gmail.com>
2020-10-06 15:54:11 +0600

    Changed on_ready event banner

--- shikamaru/plugins/events.py
+++ shikamaru/plugins/events.py
@@ -22,7 +22,16 @@ class Events(lightbulb.Plugin):
                 type=hikari.ActivityType.WATCHING,
             )
         )
-        print("\n\n I'm ready for work! What a drag.....\n\n")
+        print("""\n\n
+   _____ _                                                                                               
+  / ____| |   (_) |                                    (_)                        | |      
+ | (___ | |__  _| | ____ _ _ __ ___   __ _ _ __ _   _   _ ___   _ __ ___  __ _  __| |_   _ 
+  \___ \| '_ \| | |/ / _` | '_ ` _ \ / _` | '__| | | | | / __| | '__/ _ \/ _` |/ _` | | | |
+  ____) | | | | |   < (_| | | | | | | (_| | |  | |_| | | \__ \ | | |  __/ (_| | (_| | |_| |
+ |_____/|_| |_|_|_|\_\__,_|_| |_| |_|\__,_|_|   \__,_| |_|___/ |_|  \___|\__,_|\__,_|\__, |
+                                                                                      __/ |
+                                                                                     |___/ 
+              \n\n""")
 
     @lightbulb.listener(hikari.MessageCreateEvent)
     async def on_message(self, event):
